89439caf858e8419a093a067c0a75950ea81b539
TeonaN <teona.nikavadze.1@btu.edu.ge>
2020-05-25 06:00:42 +0400

    initial commit

---
+++ Assignment7/app/src/main/java/com/example/assignment7/ItemsAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.assignment7
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.note_item.view.*
+
+class ItemsAdapter( private var items: MutableList<String>) :
+    RecyclerView.Adapter<ItemsAdapter.ProductViewHolder>() {
+
+    class ProductViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder( parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent,false)
+        return ProductViewHolder(view)
+
+    }
+
+    override fun getItemCount(): Int = items.count()
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        val p = items[position]
+
+
+        holder.itemView.textView1.text = p
+
+
+    }
+
+}
+
---
+++ Assignment7/app/src/main/java/com/example/assignment7/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.assignment7
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+
+import androidx.recyclerview.widget.LinearLayoutManager
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    var items:MutableSet<String> = mutableSetOf()
+    private lateinit var adapter: ItemsAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val sharedPreferences = getSharedPreferences("MyApplication", Context.MODE_PRIVATE)
+        val savedText = sharedPreferences.getStringSet("NOTES", setOf<String>())
+        if (savedText != null) {
+            items = savedText.toMutableSet()
+        }
+        initRecycler(items)
+
+
+
+        addBtn.setOnClickListener {
+
+            val input = inputText.text.toString()
+
+
+            if (!TextUtils.isEmpty(input)) {
+                items.add(inputText.text.toString())
+                adapter.notifyDataSetChanged()
+
+                inputText.setText("")
+
+                sharedPreferences.edit().putStringSet("NOTES", items).apply()
+            }
+
+        }
+
+    }
+    private fun initRecycler(items: MutableSet<String>) {
+       recyclerView.layoutManager = LinearLayoutManager(this)
+       recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = ItemsAdapter(items.toMutableList())
+        recyclerView.adapter = adapter
+    }
+
+}
+
+
+
+
