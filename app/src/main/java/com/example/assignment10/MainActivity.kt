6d4fdf33a7343d153906cedaaeeb594bf715e476
TeonaN <teona.nikavadze.1@btu.edu.ge>
2020-06-19 00:06:26 +0400

    commit

---
+++ app/src/main/java/com/example/assignment10/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.assignment10
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance();
+
+
+        registerBtn.setOnClickListener {
+
+            val email = editTextTextEmailAddress.text.toString()
+            val password = editTextTextPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                        } else {
+                            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+            }
+
+        }
+
+    }
+}
+
+
